c914674a1ae00b1571e271ecc1cf5a87d71f4707
unknown <kingroma>
2021-07-12 00:25:03 +0900

    update

--- js/calendar.js
+++ js/calendar.js
@@ -2,27 +2,39 @@ var currentDate = new Date();
 
 
 $(document).ready(function(){
-    createSmallCaneldar();
+    createCaneldar();
 });
 
 function getCalendar(yyyymm){
     var result = [] ; 
     if ( yyyymm != undefined && yyyymm != null ) {
         var d = new Date(yyyymm.substr(0,4),Number(yyyymm.substr(4,2)) - 1);
+        
+        var currentMonth = d.getMonth();
 
         while ( d.getDay() != 0 ) {
             d.setDate(d.getDate() - 1)
         }
-        
 
-        for ( var i = 0 ; i < 35 ; i ++ ) {
+        var len = 35 ; 
+
+        var temp = new Date(d);
+        for ( var i = 0 ; i < 36 ; i ++ ) {
+            temp.setDate(temp.getDate() + 1)
+        }
+
+        if ( temp.getMonth() == currentMonth ) {
+            len = 45 ; 
+        }
+
+        for ( var i = 0 ; i < len ; i ++ ) {
             var yyyy = d.getFullYear();
             var mm = d.getMonth() + 1 ;
             var dd = d.getDate() ; 
             var day = d.getDay();
 
             var obj = {
-                d: d,
+                d: new Date(d),
                 yyyy:yyyy,
                 mm:mm,
                 dd:dd,
@@ -32,26 +44,33 @@ function getCalendar(yyyymm){
             result.push(obj);
             d.setDate(d.getDate() + 1)
         }
+
+        console.log(d.getMonth());
     }
     return result ; 
 }
 
 function today(){
     currentDate = new Date();
-    createSmallCaneldar();
+    createCaneldar();
 }
 
 function back(){
     currentDate.setMonth(currentDate.getMonth()-1);
-    createSmallCaneldar();
+    createCaneldar();
 }
 
 function next(){
     currentDate.setMonth(currentDate.getMonth()+1);
-    createSmallCaneldar();
+    createCaneldar();
 }
 
-function makeCalendarTd(dd,day){
+function makeCalendarTd(obj){
+    var dd = obj.dd ; 
+    var day = obj.day ; 
+
+    var current = new Date();
+
     var td = $('<td></td>')
     td.css("text-align","center")
     td.css("width","14%")
@@ -63,6 +82,11 @@ function makeCalendarTd(dd,day){
     } else if ( day == 6 ){
         td.addClass('saturday-text')
     }
+
+    if ( currentDate.getMonth() != obj.d.getMonth() ){
+        td.addClass('calendar-prev')
+    }
+
     td.addClass('calendar-td')
 
     td.append(dd)
@@ -70,7 +94,7 @@ function makeCalendarTd(dd,day){
     return td ; 
 }
 
-function createSmallCaneldar (){
+function createCaneldar (){
     var yyyy = '' + currentDate.getFullYear();
     var mm = '' + ( 
         currentDate.getMonth() + 1 >= 10 ? 
@@ -89,13 +113,13 @@ function createSmallCaneldar (){
 
     var target = $('#calendar') ;
     
-    for ( var i = 0 ; i < 5 ; i ++ ) {
+    for ( var i = 0 ; i < list.length / 7 ; i ++ ) {
         var tr = $('<tr></tr>')
         tr.addClass('calendar-row')
 
         for ( var j = 0 ; j < 7 ; j ++ ) {
             var obj = list[7*i+j];
-            var td = makeCalendarTd(obj.dd,obj.day)
+            var td = makeCalendarTd(obj)
 
             tr.append(td);
         }

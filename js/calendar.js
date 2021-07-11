cdb6b6dd81ed2fc99e17c6f69db90edc666c7ec3
unknown <kingroma>
2021-07-12 05:12:52 +0900

    update

--- js/calendar.js
+++ js/calendar.js
@@ -24,13 +24,13 @@ function getCalendar(yyyymm){
         }
 
         if ( temp.getMonth() == currentMonth ) {
-            len = 45 ; 
+            len = 42 ; 
         }
 
         for ( var i = 0 ; i < len ; i ++ ) {
             var yyyy = d.getFullYear();
-            var mm = d.getMonth() + 1 ;
-            var dd = d.getDate() ; 
+            var mm = numChangeStr2 ( d.getMonth() + 1 ) ;
+            var dd = numChangeStr2 ( d.getDate() ) ; 
             var day = d.getDay();
 
             var obj = {
@@ -50,28 +50,49 @@ function getCalendar(yyyymm){
     return result ; 
 }
 
-function today(){
-    currentDate = new Date();
-    createCaneldar();
-}
+function clickCalendarTd(obj,e){
+    
+    // e.preventDefault()
+    e.stopPropagation()
+    $('#addScheduleDiv').css("display","block")
+    console.log(obj.yyyy + '/' + obj.mm + '/' + obj.dd);
 
-function back(){
-    currentDate.setMonth(currentDate.getMonth()-1);
-    createCaneldar();
-}
+    var target = $('#calendar-'+obj.yyyy+''+obj.mm+''+obj.dd);
+    var position = target.position();
 
-function next(){
-    currentDate.setMonth(currentDate.getMonth()+1);
-    createCaneldar();
+    var top = position.top;
+    var left = 0 ; 
+    
+    if ( Number(target.attr("day")) <= 1 ){
+        left = target.width() + position.left ; 
+    } else {
+        left = position.left - 200 ; 
+    }
+
+    $('#addScheduleDiv').css("left",left).css("top",top);
 }
 
 function makeCalendarTd(obj){
+    
+    try {
+        var dd = obj.dd ;     
+    } catch (error) {
+        console.log(obj);
+        console.log(error);
+    }
     var dd = obj.dd ; 
     var day = obj.day ; 
 
     var current = new Date();
 
     var td = $('<td></td>')
+    
+    td.attr("id","calendar-"+obj.yyyy + '' + obj.mm + '' + obj.dd);
+    td.attr("yyyy",obj.yyyy);
+    td.attr("mm",obj.mm);
+    td.attr("dd",obj.dd);
+    td.attr("day",obj.day);
+
     td.css("text-align","center")
     td.css("width","14%")
     td.css('border','1px solid white')
@@ -89,6 +110,10 @@ function makeCalendarTd(obj){
 
     td.addClass('calendar-td')
 
+    td.on('click',function(e){
+        clickCalendarTd(obj,e)
+    })
+
     td.append(dd)
 
     return td ; 
@@ -96,11 +121,7 @@ function makeCalendarTd(obj){
 
 function createCaneldar (){
     var yyyy = '' + currentDate.getFullYear();
-    var mm = '' + ( 
-        currentDate.getMonth() + 1 >= 10 ? 
-        '' + ( currentDate.getMonth() + 1 ) : 
-        '0' + ( currentDate.getMonth() + 1 )
-    ) ;
+    var mm = numChangeStr2 ( currentDate.getMonth() + 1 ) ;
 
     $('.yyyy').html(yyyy);
     $('.mm').html(mm);
@@ -119,9 +140,11 @@ function createCaneldar (){
 
         for ( var j = 0 ; j < 7 ; j ++ ) {
             var obj = list[7*i+j];
-            var td = makeCalendarTd(obj)
+            if (obj != undefined ) {
+                var td = makeCalendarTd(obj)
 
-            tr.append(td);
+                tr.append(td);
+            }
         }
 
         target.append(tr);
@@ -129,4 +152,34 @@ function createCaneldar (){
 
     target.append('<tr class="calendar-empty" style="height:10px"><td colspan="7"></td></tr>')
     
+}
+
+function clearModal(){
+    console.log("clearModal")
+    $('#addScheduleDiv').css("display","none")
+}
+
+function today(){
+    currentDate = new Date();
+    createCaneldar();
+}
+
+function back(){
+    currentDate.setMonth(currentDate.getMonth()-1);
+    createCaneldar();
+}
+
+function next(){
+    currentDate.setMonth(currentDate.getMonth()+1);
+    createCaneldar();
+}
+
+function numChangeStr2(input){
+    var result = '' ; 
+    if ( Number(input) < 10 ) {
+        result = '0' + input ; 
+    } else {
+        result = input ; 
+    }
+    return result ; 
 }
